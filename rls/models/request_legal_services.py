9d49ad5af83fe21c2e59884510912f7a8a84bec2
LeoricCryotek <108315809+LeoricCryotek@users.noreply.github.com>
2023-04-03 22:49:12 -0700

    Functional
    I have an installable module with basic function levels.

--- rls/models/request_legal_services.py
+++ rls/models/request_legal_services.py
@@ -6,15 +6,19 @@ class RequestLegalServices(models.Model):
     _description = 'Request Legal Services'
     _inherit = ['mail.thread']
 
-    #name = fields.Char(required=True, string='Name', widget='Text', tracking=True)
-    #nature_of_request = fields.Selection(selection=[('contract', 'Contract'),
-                                                  #  ('dedication', 'Dedication'),
-                                                  #  ('deed', 'Deed'),
-                                                  #  ('easement', 'Easement'),
-                                                  #  ('legal_opinion', 'Legal Opinion'),
-                                                  #  ('ordinance', 'Ordinance'),
-                                                  #  ('other', 'Other'),
-                                                  #  ('resolution', 'Resolution'),
-                                                  #  ('subpoena', 'Subpoena'),
-                                                  #  ('vacation_of_r-o-w', 'Vacation of R-O-W')],
-                                        # string='Nature of Request', widget='selection', tracking=True)
+    name = fields.Char(string='Request', required=True, copy=False, default='New', tracking=True)
+    nature_of_request = fields.Selection(selection=[('contract', 'Contract'),
+                                                    ('dedication', 'Dedication'),
+                                                    ('deed', 'Deed'),
+                                                    ('easement', 'Easement'),
+                                                    ('legal_opinion', 'Legal Opinion'),
+                                                    ('ordinance', 'Ordinance'),
+                                                    ('other', 'Other'),
+                                                    ('resolution', 'Resolution'),
+                                                    ('subpoena', 'Subpoena'),
+                                                    ('vacation_of_r-o-w', 'Vacation of R-O-W')],
+                                         string='Nature of Request', widget='selection', tracking=True)
+    assigned_attorney_id = fields.Many2one('res.users', string='Attorney Assigned', domain="[(1, '=', 1)]", widget='selection', tracking=True)
+    due_date = fields.Date(string='Due Date')
+    active = fields.Boolean(default=True)
+
