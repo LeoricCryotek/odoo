250c12613df03c479d2dec2863b353257ac6f054
nerdican <dslewiston@gmail.com>
2023-04-21 11:01:24 -0700

    Refinement

--- clm/__manifest__.py
+++ clm/__manifest__.py
@@ -3,14 +3,21 @@
 {
     'name': "CLM Contract Lifecycle Management",
     'version': '1.0',
-    'depends': ['base', 'documents',],
     'author': "Danny Santiago",
     'category': 'Services',
     'license': 'LGPL-3',
     'summary': 'Manage contracts from initiation to expiration or termination',
     'description': "Contract Lifecycle Management (CLM) Module for odoo",
     'application': True,
+    'depends': ['base',
+                'documents',
+    ],
     'data': [
         'views/document_cancellation_terms_view.xml',
     ],
+    'data': [
+        # Add your other XML files here
+        'data/scheduled_action.xml',
+    ],
+    'installable': True,
 }
--- clm/models/document_cancellation_terms.py
+++ clm/models/document_cancellation_terms.py
@@ -5,22 +5,24 @@ from dateutil.relativedelta import relativedelta
 
 
 class DocumentCancellationTerms(models.Model):
-    _inherit = 'documents.document'
+    _inherit = ['documents.document']
 
-    written_notice_30 = fields.Boolean(string="30 Days Written Notice")
-    written_notice_60 = fields.Boolean(string="60 Days Written Notice")
+    cancellation_notice_enabled = fields.Boolean(string="Cancellation Notice")
+    cancellation_notice = fields.Selection([
+        ('30', '30 Days Written Notice'),
+        ('60', '60 Days Written Notice'),
+    ], string='Cancellation Notice')
     early_termination_fee = fields.Boolean(string="Early Termination Fee")
-    termination_for_convenience = fields.Boolean(string="Termination for Convenience")
+    early_termination_fee_amount = fields.Char(string="Early Termination Fee Amount")
     termination_for_cause = fields.Boolean(string="Termination for Cause")
     termination_upon_expiration = fields.Boolean(string="Termination upon Expiration")
-    termination_upon_bankruptcy = fields.Boolean(string="Termination upon Bankruptcy or Insolvency")
     termination_for_non_payment = fields.Boolean(string="Termination for Non-payment")
-    termination_upon_change_of_control = fields.Boolean(string="Termination upon Change of Control")
-    termination_for_force_majeure = fields.Boolean(string="Termination for Force Majeure")
-    mutual_termination = fields.Boolean(string="Mutual Termination")
     start_date = fields.Date(string='Start Date')
     termination_date = fields.Date(string='Termination Date', store=True)
-    days_left = fields.Integer(string='Days Left', compute='_compute_days_left', store=True)
+    days_left_to_termination = fields.Integer(string='Days Left to Termination',
+                                              compute='_compute_days_left_to_termination', store=True)
+    days_left_to_cancel = fields.Integer(string='Days Left to Cancel', compute='_compute_days_left_to_cancel',
+                                         store=True)
     term = fields.Selection([
         ('12', '12 Month'),
         ('24', '24 Month'),
@@ -47,27 +49,52 @@ class DocumentCancellationTerms(models.Model):
         for record in self:
             record.termination_date.readonly = record.term != 'other'
 
-    @api.depends('termination_date')
-    def _compute_days_left(self):
-        for record in self:
-            if record.termination_date:
-                today = fields.Date.today()
-                days_left = (record.termination_date - today).days
-                record.days_left = days_left - 1
-            else:
-                record.days_left = 0
-
-    @api.depends('termination_date', 'written_notice_30', 'written_notice_60')
+    @api.depends('termination_date', 'cancellation_notice')
     def _compute_cancellation_deadline(self):
         for record in self:
             if record.termination_date:
-                if record.written_notice_30:
+                if record.cancellation_notice == '30':
                     record.cancellation_deadline = record.termination_date - relativedelta(days=30)
-                elif record.written_notice_60:
+                elif record.cancellation_notice == '60':
                     record.cancellation_deadline = record.termination_date - relativedelta(days=60)
                 else:
                     record.cancellation_deadline = record.termination_date
             else:
                 record.cancellation_deadline = False
 
+    @api.depends('termination_date')
+    def _compute_days_left_to_termination(self):
+        for record in self:
+            if record.termination_date:
+                today = fields.Date.today()
+                days_left_to_termination = (record.termination_date - today).days
+                record.days_left_to_termination = days_left_to_termination - 1
+            else:
+                record.days_left_to_termination = 0
+
+    @api.depends('cancellation_deadline')
+    def _compute_days_left_to_cancel(self):
+        for record in self:
+            if record.cancellation_deadline:
+                today = fields.Date.today()
+                days_left_to_cancel = (record.cancellation_deadline - today).days
+                record.days_left_to_cancel = days_left_to_cancel - 1
+            else:
+                record.days_left_to_cancel = 0
+
+    @api.model
+    def update_days_left(self):
+        today = fields.Date.today()
+        docs = self.search([])
+
+        # Filter out records with null or zero values for the days_left_to_termination and days_left_to_cancel fields
+        docs = docs.filtered(lambda d: d.days_left_to_termination and d.days_left_to_cancel)
+
+        for doc in docs:
+            days_left_to_termination = (doc.deadline_to_termination - today).days
+            days_left_to_cancel = (doc.deadline_to_cancel - today).days
+            doc.write({
+                'days_left_to_termination': days_left_to_termination,
+                'days_left_to_cancel': days_left_to_cancel,
+            })
     # Add other fields for cancellation terms as needed
